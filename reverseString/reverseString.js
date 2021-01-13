18f1b5ead7f2d9e6aa9952c7dfe57730758b1699
jonathanyu1 <jonathanyusir@gmail.com>
2021-01-12 19:45:42 -0500

    Finished reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string1) {
+    let string2=``;
+    for (i=1;i<=string1.length;i++){
+        string2+=string1[string1.length-i];
+    }
+    return string2;
 }
 
 module.exports = reverseString
