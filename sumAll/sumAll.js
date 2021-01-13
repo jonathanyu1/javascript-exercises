db4975378bb1e6439bc9982ce02dec90db3d35b5
jonathanyu1 <jonathanyusir@gmail.com>
2021-01-13 16:03:43 -0500

    finished sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum=0;
+    if (num1>=0 && num2>=0 && typeof(num1)== `number` && typeof(num2)== `number`){
+        for (i=Math.min(num1,num2);i<=Math.max(num1,num2);i++){
+           finalSum+=i;
+       }
+    } else{
+        return `ERROR`;
+    }
+    return finalSum;
 }
 
 module.exports = sumAll
+
+// jasmine sumAll.spec.js
