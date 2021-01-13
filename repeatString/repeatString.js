89aeb178458ed059f1ea8a28838a9b07f56f0be0
jonathanyu1 <jonathanyusir@gmail.com>
2021-01-12 19:36:57 -0500

    finished helloWorld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string1, repeatTimes) {
+    let i=0;
+    let newString=``;
+    if (repeatTimes<i){
+        return `ERROR`;
+    } else {
+        while (i<repeatTimes){
+            newString+=string1;
+            i++;
+        }
+    return newString;
+    }
 }
 
 module.exports = repeatString
