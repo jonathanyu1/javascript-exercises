12e9db75fddcfeae94d6d8418185e2b8f329b4f4
jonathanyu1 <jonathanyusir@gmail.com>
2021-01-13 15:39:08 -0500

    finished removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array1, ...theArgs) {
+    for (i=0;i<=theArgs.length;i++){
+        if (array1.indexOf(theArgs[i])==-1){
+            continue;
+        }
+        else{
+            array1.splice(array1.indexOf(theArgs[i]),1);
+        }
+    }
+    return array1;
 }
 
 module.exports = removeFromArray
+
+// jasmine removeFromArray.spec.js
