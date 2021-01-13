811c1dddb7d3fa0ed00d076941b39835b2e6a784
jonathanyu1 <jonathanyusir@gmail.com>
2021-01-13 16:39:14 -0500

    finished tempConversion.js

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,22 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  finalTemp = ((temp-32)*5/9);
+  if (Number.isInteger(finalTemp)){
+    return finalTemp;
+  }  
+  return Number(finalTemp.toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  finalTemp = (temp*9/5)+32;
+  if (Number.isInteger(finalTemp)){
+    return finalTemp;
+  }  
+  return Number(finalTemp.toFixed(1));
 }
 
 module.exports = {
   ftoc,
   ctof
 }
+
+// jasmine tempConversion.spec.js
