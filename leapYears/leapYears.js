a93b91997c7fa8a6ff7c1251a8bab986713af3ea
jonathanyu1 <jonathanyusir@gmail.com>
2021-01-13 16:25:18 -0500

    finished leapYears.js

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%100==0){
+        console.log(year%400==0 ? true:false);
+        return (year%400==0 ? true:false);
+    }
+    return (year%4==0);
 }
 
 module.exports = leapYears
+
+// jasmine leapYears.spec.js
